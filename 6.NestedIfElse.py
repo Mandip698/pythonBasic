c53a9d6032c0e107fbf4a0bf2e8c902b933a0878
Mandip698 <mandip698@gmail.com>
2022-05-06 08:54:03 +0545

    update

--- 6.NestedIfElse.py
+++ 6.NestedIfElse.py
@@ -14,18 +14,36 @@
 #         print('z is greater than x and y')
 
 
-username = 'admin'
-password = '12345'
-if username == 'admin':
-    if password == '12345':
-        print('You are logged in')
-    else:
-        print('Wrong Password')
-else:
-    if password == '12345':
-        if username == 'admin':
-            print('You are logged in')
-        else:
-            print('Wrong username')
-    else:
-        print('Wrong username and password')
+# username = 'admin'
+# password = '12345'
+# if username == 'admin':
+#     if password == '12345':
+#         print('You are logged in')
+#     else:
+#         print('Wrong Password')
+# else:
+#     if password == '12345':
+#         if username == 'admin':
+#             print('You are logged in')
+#         else:
+#             print('Wrong username')
+#     else:
+#         print('Wrong username and password')
+
+
+# even and odd number
+
+# n = 10
+# if n % 2 == 0:
+#     print('Even number')
+# else:
+#     print('Odd number')
+
+# WAP to check if 3 and 5 are multiples
+
+# n = int(input("Enter number :"))
+# if n % 3 == 0 and n % 5 == 0:
+#     print(f'3 and 5 are multiples of {n}')
+# else:
+#     print(f'3 and 5 are not multiples of {n} ')
+
