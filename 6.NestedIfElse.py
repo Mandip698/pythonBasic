2db3c37aaf908d38dd932514f88c4e70359872d2
Mandip698 <mandip698@gmail.com>
2022-05-06 08:38:42 +0545

    update

--- 6.NestedIfElse.py
+++ 6.NestedIfElse.py
@@ -1,3 +1,31 @@
-x = 10
-y = 20
-z = 30
+# x = 10
+# y = 20
+# z = 30
+# # nested if else statement
+# if x<y:
+#     if x>z:
+#         print('x is greater than y and z')
+#     else:
+#         print('z is greater than x and y')
+# else:
+#     if y>z:
+#         print('y is greater than z')
+#     else:
+#         print('z is greater than x and y')
+
+
+username = 'admin'
+password = '12345'
+if username == 'admin':
+    if password == '12345':
+        print('You are logged in')
+    else:
+        print('Wrong Password')
+else:
+    if password == '12345':
+        if username == 'admin':
+            print('You are logged in')
+        else:
+            print('Wrong username')
+    else:
+        print('Wrong username and password')
