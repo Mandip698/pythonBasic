d35dae912a9c33e053953ef6f09d5884eea296c0
Mandip698 <mandip698@gmail.com>
2022-05-08 21:45:11 +0545

    update

---
+++ Exercises/Small&Large.py
@@ -0,0 +1,7 @@
+numbers = []
+for i in range(5):
+    num = int(input('Enter the number: '))
+    numbers.append(num)
+
+print("Largest number ins the list is :", max(numbers))
+print("Smallest number ins the list is :", min(numbers))
