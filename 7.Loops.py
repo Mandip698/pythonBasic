b2e2d38e10f2cdf2b279709addd10103ac1d548e
Mandip698 <mandip698@gmail.com>
2022-05-09 08:24:26 +0545

    update

--- 7.Loops.py
+++ 7.Loops.py
@@ -41,5 +41,48 @@
 # for a in data:
 #     print(a)
 
+# Print 1 to 10 in descending order
+# a = 10
+# while a > 0:
+#     print(a)    #print(a, end=" ")
+#     a -= 1
 
+# Stop iteration : break, continue
+# break
+# x = 1
+# while x <= 10:
+#     if x == 5:
+#         break
+#
+#     print(x, end=" ")
+#     x += 1
 
+# continue
+# x = 1
+# while x <= 10:
+#     x += 1
+#     if x == 5:
+#         continue
+#
+#     print(x, end=" ")
+
+# print 1 to 10 except 1,4, and 9.
+# x = 1
+# while x < 10:
+#     x += 1
+#     if x == 1 or x == 4 or x == 9:
+#         continue
+#
+#     print(x, end=" ")
+
+#
+
+num = int(input("Enter no. of students: "))
+i = 1
+students_name = []
+while i <= num:
+    name = input("Enter name: ")
+    students_name.append(name)
+    i += 1
+
+print(students_name)
